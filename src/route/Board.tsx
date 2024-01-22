474a9c27cb14e7e3b55647e4aed20bbfb0d7902f
sebastien <miantamag@gmail.com>
2024-01-22 18:02:47 +0100

    update

--- src/route/Board.tsx
+++ src/route/Board.tsx
@@ -1,4 +1,4 @@
-import {useNavigate, useParams} from "react-router-dom";
+import { useNavigate, useParams} from "react-router-dom";
 import { Navbar } from "../Navbar.tsx";
 import {useEffect, useState} from "react";
 import Jwt from "../jwt/Jwt.tsx";
@@ -10,11 +10,26 @@ type Board = {
   description: string;
   author:string;
   datetime:string
+};
+type List = {
+  id: number;
+  name: string;
+};
+
+type Card = {
+  id: number;
+  name: string;
+  description: string;
+  importance: number;
+  list: number;
 
 };
 function Board() {
   const { id } = useParams();
   const [board, setBoard] = useState<Board>();
+  const[lists, setList] = useState<List[]>([]);
+  const[cards, setCard] = useState<Card[]>([]);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,34 +37,56 @@ function Board() {
       const token = await Jwt();
       if (token) {
         try {
-          const response = await axios.get("https://django.miantsebastien.com/api/board/"+id+"/", {
-            headers: {'Authorization': `Bearer ${token}`}
+          const responseBoard = await axios.get(`https://django.miantsebastien.com/api/board/${id}/`, {
+            headers: { 'Authorization': `Bearer ${token}` }
           });
-          setBoard(response.data);
+          setBoard(responseBoard.data);
 
-      } catch (error) {
-        alert("error")
+          const responseList = await axios.get(`https://django.miantsebastien.com/api/board/${id}/lists/`, {
+            headers: { 'Authorization': `Bearer ${token}` }
+          });
+          setList(responseList.data);
+
+          const cardPromises = responseList.data.map(async (list: { id: never; }) => {
+            try {
+              const responseCard = await axios.get(`https://django.miantsebastien.com/api/list/${list.id}/cards/`, {
+                headers: { 'Authorization': `Bearer ${token}` }
+              });
+              return responseCard.data;
+            } catch (error) {
+              console.log(error);
+              return [];
+            }
+          });
+
+          const cardResults = await Promise.all(cardPromises);
+
+          setCard(cardResults.flat());
+        } catch (error) {
+          console.log(error);
+        }
       }
-    }
+
       else {
         navigate("/login");
 
       }
     }
 
-
     fetchData();
   }, [id, navigate]);
 
   return (
     <>
+
+
       <Navbar></Navbar>
       <section className="board-info-bar">
 
         <div className="board-controls">
 
           <button className="board-title btn">
-            <h2>{board?.name}</h2>
+            <h2 >{board?.name}</h2>
           </button>
 
           <button className="star-btn btn" aria-label="Star Board">
@@ -60,32 +97,23 @@ function Board() {
       </section>
       <section className="lists-container">
 
-        <div className="list">
+        {lists.map((list) => (
+          <>
+            <div className="list" >
 
-          <h3 className="list-title">Tasks to Do</h3>
+              <h3 className="list-title" key={list.id}>{list.name}</h3>
 
-          <ul className="list-items">
-            <li>Complete mock-up for client website</li>
-            <li>Email mock-up to client for feedback</li>
-            <li>Update personal website header background image</li>
-            <li>Update personal website heading fonts</li>
-            <li>Add google map to personal website</li>
-            <li>Begin draft of CSS Grid article</li>
-            <li>Read new CSS-Tricks articles</li>
-            <li>Read new Smashing Magazine articles</li>
-            <li>Read other bookmarked articles</li>
-            <li>Look through portfolios to gather inspiration</li>
-            <li>Create something cool for CodePen</li>
-            <li>Post latest CodePen work on Twitter</li>
-            <li>Listen to new Syntax.fm episode</li>
-            <li>Listen to new CodePen Radio episode</li>
-          </ul>
-
-          <button className="add-card-btn btn">Add a card</button>
-
-        </div>
-
-
+              <ul className="list-items">
+                {cards
+                  .filter((card) => card.list === list.id)
+                  .map((filteredCard) => (
+                    <li key={filteredCard.id}>{filteredCard.name}</li>
+                  ))}
+              </ul>
+              <button className="add-card-btn btn">Add a card</button>
+            </div>
+          </>
+        ))}
         <button className="add-list-btn btn">Add a list</button>
 
       </section>
