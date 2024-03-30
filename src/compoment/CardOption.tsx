1cc1f4d1d0df04639f822b5c2a87a0ee803ea3f7
sebastien <miantamag@gmail.com>
2024-03-30 21:48:05 +0100

    trello add

--- src/compoment/CardOption.tsx
+++ src/compoment/CardOption.tsx
@@ -5,9 +5,12 @@ interface CardOptionProps {
   setShowOverlayCard: (value: boolean) => void;
   selectedCardName: string;
   selectedCardId: number
+  selectedCardImportance: number
+  selectedCardDescription: string
+
 }
 
-export function CardOption({ showOverlayCard, setShowOverlayCard, selectedCardName,selectedCardId }: CardOptionProps) {
+export function CardOption({ showOverlayCard, setShowOverlayCard, selectedCardName,selectedCardId,selectedCardImportance,selectedCardDescription }: CardOptionProps) {
   const toggleOverlay = () => {
     setShowOverlayCard(!showOverlayCard); // Modifier l'état dans ListOption
   };
@@ -17,13 +20,18 @@ export function CardOption({ showOverlayCard, setShowOverlayCard, selectedCardNa
     <div>
       {showOverlayCard && (
         <div className="overlay">
-          <h2>{selectedCardName}</h2> {/* Afficher le nom de la liste sélectionnée */}
+          <h2>titre: {selectedCardName}</h2>
+          <h3>description: {selectedCardDescription}</h3>
+          <h3>importance: {selectedCardImportance}</h3>
           <ul className="list-items btn">
             <Link className="star-btn btn no-decoration" to={`/board/${id}/list/${selectedCardId}/delete`}>
-              <li>supprimer la list</li>
+              <li>supprimer la card</li>
             </Link>
             <Link className="star-btn btn no-decoration" to={`/board/${id}/list/${selectedCardId}/update`}>
-              <li>modifier la list</li>
+              <li>modifier la card</li>
+            </Link>
+            <Link className="star-btn btn no-decoration" to={`/board/${id}/list/${selectedCardId}/update`}>
+              <li>deplacer la card vers </li>
             </Link>
           </ul>
           <button onClick={toggleOverlay} className="btn">Fermer</button>
